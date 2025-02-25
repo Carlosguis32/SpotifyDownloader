4076cc8c96d84a7cafaaacae0ca05229e9dc8f56
Carlos Pastor Gimeno <cpastorgimeno@gmail.com>
2025-02-25 20:47:37 +0100

    v0.0.2 - Downloading playlists songs implemented

--- spotify_downloader/src/App.jsx
+++ spotify_downloader/src/App.jsx
@@ -1,13 +1,40 @@
 import { useState } from "react";
 import "./App.css";
+import { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, YOUTUBE_API_KEY, YOUTUBE_URL_PREFIX } from "./parameters";
 
 function App() {
-	const [url, setUrl] = useState("");
+	const [playlistId, setPlaylistId] = useState("");
+	const [spotifyToken, setSpotifyToken] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
 
-	const downloadSong = async (url) => {
+	async function getSpotifyToken() {
+		try {
+			const response = await fetch("https://accounts.spotify.com/api/token", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/x-www-form-urlencoded",
+					Authorization: "Basic " + btoa(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`),
+				},
+				body: "grant_type=client_credentials",
+			});
+
+			if (!response.ok) {
+				throw new Error("Failed to get Spotify token");
+			}
+
+			const data = await response.json();
+			setSpotifyToken(data.access_token);
+			return data.access_token;
+		} catch (error) {
+			console.error("Error getting Spotify token:", error);
+			throw error;
+		}
+	}
+
+	async function downloadSong(url) {
 		try {
 			setIsLoading(true);
+			console.log(url);
 			const apiUrl = `http://localhost:4000/download`;
 			const params = new URLSearchParams({ url });
 
@@ -17,43 +44,111 @@ function App() {
 				const errorData = await response.json();
 				throw new Error(errorData.error || "Download failed");
 			}
-
-			alert("Download completed! Check your Desktop/SpotifyDownloads folder");
 		} catch (error) {
 			console.error("Download failed:", error);
 			alert("An error occurred while downloading the video. Please try again.");
 		} finally {
 			setIsLoading(false);
 		}
-	};
+	}
 
-	const handleDownload = async () => {
-		if (!url) {
-			alert("Please enter a URL");
-			return;
+	async function getFirstVideoURL(searchInput) {
+		if (!YOUTUBE_API_KEY) {
+			throw new Error("No API key is provided");
 		}
+
+		const url = `https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&type=video&part=id&maxResults=1&q=${encodeURIComponent(
+			searchInput
+		)}`;
 
 		try {
-			await downloadSong(url);
+			const response = await fetch(url);
+			if (!response.ok) {
+				throw new Error("Error fetching data from YouTube API");
+			}
+
+			const data = await response.json();
+
+			if (!data.items || data.items.length === 0) {
+				throw new Error("No videos found");
+			}
+
+			return `${YOUTUBE_URL_PREFIX}${data.items[0].id.videoId}`;
 		} catch (error) {
-			console.error("Error downloading:", error);
-			alert("Failed to download. Please check the URL and try again.");
+			console.error("YouTube API Error:", error.message);
+			return null;
 		}
-	};
+	}
+
+	async function getSpotifyPlaylistData(fetchURL) {
+		try {
+			let token = spotifyToken;
+			if (!token) {
+				token = await getSpotifyToken();
+			}
+
+			const playlistIdMatch = playlistId.match(/playlist\/([a-zA-Z0-9]+)/);
+			const actualPlaylistId = playlistIdMatch ? playlistIdMatch[1] : playlistId;
+
+			let apiUrl;
+
+			if (fetchURL) {
+				apiUrl = fetchURL;
+			} else {
+				apiUrl = `https://api.spotify.com/v1/playlists/${actualPlaylistId}/tracks`;
+			}
+			const response = await fetch(apiUrl, {
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "application/json",
+				},
+			});
+
+			if (!response.ok) {
+				if (response.status === 401) {
+					token = await getSpotifyToken();
+					return getSpotifyPlaylistData();
+				}
+				const errorData = await response.json();
+				throw new Error(errorData.error.message || "Failed to fetch playlist");
+			}
+
+			const data = await response.json();
+
+			for (const item of data.items) {
+				const videoUrl = await getFirstVideoURL(
+					`${item.track.artists[0].name} - ${item.track.name} - Album: ${item.track.album.name}`
+				);
+
+				await downloadSong(videoUrl);
+			}
+
+			if (data.next) {
+				await getSpotifyPlaylistData(data.next);
+			}
+		} catch (error) {
+			console.error("Error fetching playlist:", error);
+			alert("Failed to fetch Spotify playlist. Please check the playlist ID and try again.");
+		} finally {
+			setIsLoading(false);
+		}
+	}
 
 	return (
 		<div className="container">
-			<h1>Youtube Downloader</h1>
-			<input
-				type="text"
-				placeholder="Enter url"
-				value={url}
-				onChange={(e) => setUrl(e.target.value)}
-				disabled={isLoading}
-			/>
-			<button onClick={handleDownload} disabled={isLoading}>
+			<h1>Spotify Downloader</h1>
+
+			<button onClick={() => getSpotifyPlaylistData()} disabled={isLoading}>
 				{isLoading ? "Downloading..." : "Download"}
 			</button>
+
+			<input
+				type="text"
+				placeholder="Enter playlist url"
+				onChange={(e) => setPlaylistId(e.target.value)}
+				disabled={isLoading}
+			/>
 		</div>
 	);
 }
---
+++ spotify_downloader/src/parameters.js
@@ -0,0 +1,4 @@
+export const YOUTUBE_API_KEY = "AIzaSyC07Hm0phbZacssUvUKafdeoJkmSwTUYkc";
+export const YOUTUBE_URL_PREFIX = "https://www.youtube.com/watch?v=";
+export const SPOTIFY_CLIENT_ID = "ea55462b6275483da05b1c66671bff21";
+export const SPOTIFY_CLIENT_SECRET = "8496fdc443c44e86b9fbded2a5ab13d5";
